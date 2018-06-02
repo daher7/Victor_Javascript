55a2f65146c9756e67a2d09c5aa0ee0f08220c08
daher7 <daher7@gmail.com>
2018-06-02 13:52:39 +0200

    validacion formulario

--- js/11_ejercicio_formulario.js
+++ js/11_ejercicio_formulario.js
@@ -11,8 +11,8 @@ function cargarDom() {
     console.log("DOM cargado"); // Comprobamos que el DOM se ha cargado.
     formulario = document.querySelector("#formulario");
     box_dashed = document.querySelector(".dashed");
-    formulario.addEventListener('submit', recogerDatos);
-    box_dashed.style.visibility = "hidden";  // Al cargar la pagina, este div está oculto.
+    formulario.addEventListener('submit', recogerDatos); // Nada nas cargar la pagina, llamo a la funcion.
+    box_dashed.style.visibility = "hidden"; // Al cargar la pagina, este div está oculto.
     //box_dashed.style.display = "none";
 }
 
@@ -22,22 +22,51 @@ function recogerDatos() {
     // Recogemos los datos del formulario.
     nombre = document.querySelector("#nombre").value;
     apellidos = document.querySelector("#apellidos").value;
-    edad = document.querySelector("#edad").value;
+    edad = parseInt(document.querySelector("#edad").value);
+
+    validarDatos();
+}
+
+function validarDatos() {
+
+    if (nombre.trim() == null || nombre.trim().length == 0) {
+        alert("El nombre no es correcto");
+        return false;
+    }
+    if (apellidos.trim() == null || apellidos.trim().length == 0) {
+        alert("Los apellidos no son correctos");
+        return false;
+    }
+    if (edad == null || edad <= 0 || isNaN(edad)) {
+        alert("La edad no es correcta");
+        return false;
+    }
     console.log(nombre, apellidos, edad);
+    // Lo pintamos
     imprimirDatos();
 }
 
 function imprimirDatos() {
-
+    /*
     //box_dashed.style.display = "block";
-    box_dashed.style.visibility = "visible";    // Ahora sí mostramos el div oculto previamente.
-    var datos_usuario = [nombre, apellidos, edad];  // Arary para meter los datos.
+       var datos_usuario = [nombre, apellidos, edad]; // Arary para meter los datos.
     for (let indice in datos_usuario) {
         var parrafo = document.createElement("p");
         parrafo.append(datos_usuario[indice]);
         box_dashed.append(parrafo);
-    }
+    }*/
+
+    box_dashed.style.visibility = "visible"; // Ahora sí mostramos el div oculto previamente.
+
+    var p_nombre = document.querySelector("#p_nombre span");
+    var p_apellidos = document.querySelector("#p_apellidos span");
+    var p_edad = document.querySelector("#p_edad span");
+
+    p_nombre.innerHTML = nombre;
+    p_apellidos.innerHTML = apellidos;
+    p_edad.innerHTML = edad;
 }
+
 
 // Cargamos el DOM al cargar la página para poder trabajar con el script en el head y no en el body.
 window.addEventListener('load', cargarDom);
